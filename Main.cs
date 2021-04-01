a72f0f6fad32bd4691b7c33c4fe8f178f1aba515
xoffmod <79679205+xoffmod@users.noreply.github.com>
2021-04-01 04:24:55 +0200

    Add project files

---
+++ Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityModManagerNet;
+
+namespace SortByCategory
+{
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw("Sort by price within a category")]
+        public bool SortByPrice = true;
+
+        [Draw("More categories")]
+        public bool MoreCategories = true;
+
+        [Draw("Sort junk (bottom)")]
+        public bool SortJunk = true;
+
+        [Draw("Sell junk in mass sale")]
+        public bool JunkMassSale = false;
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+
+        void IDrawable.OnChange()
+        {
+        }
+    }
+
+#if DEBUG
+    [EnableReloading]
+#endif
+    public class Main
+    {
+        public static HarmonyLib.Harmony HarmonyInstance;
+
+        public static bool Enabled = true;
+
+        public static Settings Settings = new Settings();
+
+        public static UnityModManager.ModEntry.ModLogger Logger;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+
+            Logger = modEntry.Logger;
+            modEntry.OnGUI = new Action<UnityModManager.ModEntry>(Main.OnGUI);
+            modEntry.OnSaveGUI = new Action<UnityModManager.ModEntry>(Main.OnSaveGUI);
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            modEntry.OnUnload = new Func<UnityModManager.ModEntry, bool>(Main.OnUnload);
+
+            HarmonyInstance = new HarmonyLib.Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(typeof(Main).Assembly);
+
+            return true;
+        }
+
+        static bool OnUnload(UnityModManager.ModEntry modEntry)
+        {
+#if DEBUG
+            HarmonyInstance.UnpatchAll(null);
+#endif
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Draw(modEntry);
+        }
+
+        public static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Save(modEntry);
+        }
+    }
+}
---
+++ SortByCategory.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Linq;
+
+namespace SortByCategory
+{
+    public static class SortByCategory
+    {
+        enum CustomItemType
+        {
+            None,
+            Junk,
+        }
+
+        private static CustomItemType GetCustomItemType(Kingmaker.Items.ItemEntity item)
+        {
+            if (!Main.Settings.SortJunk || item.Blueprint.IsNotable || (item.Cost == 0 && item.TotalWeight == 0))
+            {
+                return CustomItemType.None;
+            }
+
+            if (item.Blueprint.MiscellaneousType != Kingmaker.Blueprints.Items.BlueprintItem.MiscellaneousItemType.None)
+            {
+                return CustomItemType.Junk;
+            }
+
+            if (!(item.Blueprint is Kingmaker.Blueprints.Items.Equipment.BlueprintItemEquipment))
+            {
+                if (item.Blueprint.TrashLootTypes.Length > 0)
+                {
+                    return CustomItemType.Junk;
+                }
+            }
+
+            return CustomItemType.None;
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.UI.Common.UIUtilityItem), "GetItemType")]
+        public static class UIUtilityItem_GetItemType_Patch
+        {
+            public static void Postfix(Kingmaker.Items.ItemEntity item, ref string __result)
+            {
+                if (Main.Enabled && __result == "")
+                {
+                    if (GetCustomItemType(item) == CustomItemType.Junk)
+                    {
+                        __result = "Junk";
+                        return;
+                    }
+
+                    if (Main.Settings.MoreCategories)
+                    {
+                        if (Kingmaker.Blueprints.BlueprintExtenstions.GetComponent<Kingmaker.Blueprints.Items.Components.CopyRecipe>(item.Blueprint))
+                        {
+                            __result = "Recipe";
+                        }
+                        else if (item.Blueprint is Kingmaker.Blueprints.Items.BlueprintItemKey)
+                        {
+                            __result = "Key";
+                        }
+                        else if (item.Blueprint.name.StartsWith("Artifact_"))
+                        {
+                            __result = "Antique artifact";
+                        }
+                        else if (item.Blueprint.name.StartsWith("Antique_"))
+                        {
+                            __result = "Antique relic";
+                        }
+                        else if (item.Blueprint.name.StartsWith("Memento_"))
+                        {
+                            __result = "Antique";
+                        }
+                        else if (item.Blueprint.IsNotable)
+                        {
+                            return;
+                        }
+
+                        if (item.Blueprint.FlavorText.Contains("cooking ingredient"))
+                        {
+                            __result = "Cooking ingredient";
+                        }
+                        else if (item.Blueprint is Kingmaker.Blueprints.Items.BlueprintItemNote)
+                        {
+                            __result = "Book";
+                        }
+                        else
+                        {
+                            switch (item.Blueprint.ItemType)
+                            {
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Weapon:
+                                    __result = "Weapon";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Shield:
+                                    __result = "Shield";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Armor:
+                                    __result = "Armor";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Ring:
+                                    __result = "Ring";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Belt:
+                                    __result = "Belt";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Feet:
+                                    __result = "Boots";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Gloves:
+                                    __result = "Gloves";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Head:
+                                    __result = "Headgear";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Neck:
+                                    __result = "Amulet";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Shoulders:
+                                    __result = "Cloak";
+                                    break;
+                                case Kingmaker.UI.Common.ItemsFilter.ItemType.Wrist:
+                                    __result = "Bracelet";
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.UI.Common.ItemsFilter), "CompareByTypeAndName")]
+        public static class ItemsFilter_CompareByTypeAndName_Patch
+        {
+            public static bool Prefix(Kingmaker.Items.ItemEntity a, Kingmaker.Items.ItemEntity b, Kingmaker.UI.Common.ItemsFilter.FilterType filter, ref int __result)
+            {
+                if (Main.Enabled)
+                {
+                    if (Kingmaker.UI.Common.ItemsFilter.GetItemType(a, filter) >= Kingmaker.UI.Common.ItemsFilter.ItemType.Other ||
+                        Kingmaker.UI.Common.ItemsFilter.GetItemType(b, filter) >= Kingmaker.UI.Common.ItemsFilter.ItemType.Other)
+                    {
+                        return true;
+                    }
+
+                    int customCompare = GetCustomItemType(a).CompareTo(GetCustomItemType(b));
+                    if (customCompare != 0)
+                    {
+                        __result = customCompare;
+                        return false;
+                    }
+
+                    if (Kingmaker.UI.Common.ItemsFilter.GetItemType(a, filter) == Kingmaker.UI.Common.ItemsFilter.GetItemType(b, filter))
+                    {
+                        int categoryCompare = Kingmaker.UI.Common.UIUtilityItem.GetItemType(a).CompareTo(Kingmaker.UI.Common.UIUtilityItem.GetItemType(b));
+                        if (categoryCompare == 0)
+                        {
+                            int priceCompare = Main.Settings.SortByPrice ? a.Cost.CompareTo(b.Cost) : 0;
+                            if (priceCompare == 0)
+                            {
+                                __result = string.Compare(a.Name, b.Name, StringComparison.Ordinal);
+
+                            }
+                            else
+                            {
+                                __result = priceCompare;
+
+                            }
+                        }
+                        else
+                        {
+                            __result = categoryCompare;
+                        }
+
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.UI.Vendor.VendorMassSale), "WantToBeSold")]
+        public static class VendorMassSale_WantToBeSold_Patch
+        {
+            public static void Postfix(Kingmaker.UI.Vendor.VendorMassSale __instance, Kingmaker.Items.ItemEntity item, ref bool __result)
+            {
+                if (Main.Enabled && Main.Settings.JunkMassSale && !__result && item != null && GetCustomItemType(item) == CustomItemType.Junk)
+                {
+                    __result = true;
+                }
+            }
+        }
+    }
+}
